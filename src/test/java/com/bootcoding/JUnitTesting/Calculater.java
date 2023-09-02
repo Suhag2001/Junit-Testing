611335b009310f5c8f1addc8cc89bd9af9107c9f
= <=>
2023-09-02 10:49:43 +0530

    first commit

---
+++ src/test/java/com/bootcoding/JUnitTesting/Calculater.java
@@ -0,0 +1,19 @@
+package com.bootcoding.JUnitTesting;
+
+public class Calculater {
+    //adddition
+    public int addTo(int a , int b , int c){
+        return a+b+c ;
+    }
+    //multiplication
+    public  int mulTo(int a , int b){
+        return  a*b ;
+    }
+
+    //compair the numbers
+    public  boolean compareTwoNums(int a , int b){
+        return  a==b ;
+    }
+
+
+}
---
+++ src/test/java/com/bootcoding/JUnitTesting/JUnitTestingApplicationTests.java
@@ -0,0 +1,40 @@
+package com.bootcoding.JUnitTesting;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestingApplicationTests {
+
+	private  Calculater c = new Calculater();
+	@Test
+	@Disabled
+	void contextLoads() {
+
+	}
+
+	@Test
+	void testSum(){
+		int expectedResult = 15;
+		int actualResult = c.addTo(5,5,5);
+		Assertions.assertThat(actualResult).isEqualTo(expectedResult);
+	}
+
+	@Test
+	void testProd(){
+		int expectedResult = 20;
+		int actualResult = c.mulTo(5, 4);
+		Assertions.assertThat(actualResult).isEqualTo(expectedResult);
+	}
+
+	@Test
+	void  compairNums(){
+		boolean actualResult = c.compareTwoNums(2, 2);
+		Assertions.assertThat(actualResult).isTrue();
+	}
+
+
+
+}
