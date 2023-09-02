d5b2184ff1e2a40ed4c114204a62d913ac847036
= <=>
2023-09-02 11:53:27 +0530

    ArrayTest file changes

--- src/main/java/com/bootcoding/JUnitTesting/array/ArryaSum.java
+++ src/main/java/com/bootcoding/JUnitTesting/array/ArryaSum.java
@@ -1,5 +1,7 @@
 package com.bootcoding.JUnitTesting.array;
 
+import java.util.ArrayList;
+
 public class ArryaSum {
     public int getSum(int numbers[]){
         int sum =0 ;
@@ -8,5 +10,35 @@ public class ArryaSum {
         }
         return sum;
     }
+
+    public int getMax(int numbers[]){
+        int n =numbers[0];
+        for (int num: numbers){
+            if(n<num){
+                n= num;
+            }
+        }
+        return n;
+    }
+
+    public int[] getEvenArray(int numbers[]){
+
+        int evenCount=0;
+        for(int num: numbers){
+            if(num%2==0){
+                evenCount++;
+            }
+        }
+        int newArray[]= new int[evenCount];
+        int i =0 ;
+        for (int num : numbers){
+           if (num%2 ==0){
+               newArray[i++]=num;
+           }
+
+        }
+        return newArray;
+    }
+
 
 }
--- src/test/java/com/bootcoding/JUnitTesting/array/ArryaSumTest.java
+++ src/test/java/com/bootcoding/JUnitTesting/array/ArryaSumTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ArryaSumTest {
-
+    ArryaSum arryaSum = new ArryaSum();
     @Test
     public void addTest(){
-        ArryaSum arryaSum = new ArryaSum();
+//        ArryaSum arryaSum = new ArryaSum();
         int[] arr = {1,2};
         int actulValue = arryaSum.getSum(arr);
         int expectedValue=3;
@@ -18,11 +18,28 @@ public class ArryaSumTest {
 
     @Test
     public void addTest2(){
-        ArryaSum arryaSum = new ArryaSum();
+//        ArryaSum arryaSum = new ArryaSum();
         int[] arr = {-1,-2};
         int actulValue = arryaSum.getSum(arr);
         int expectedValue= -3;
         assertEquals(actulValue,expectedValue);
+    }
+
+    @Test
+    public void testMax(){
+//        ArryaSum arryaSum = new ArryaSum();
+        int[] arr={1,2,3};
+        int actulValue = arryaSum.getMax(arr);
+        int expectedValue=3;
+        assertEquals(actulValue,expectedValue);
+    }
+    @Test
+    public void testEvenArray(){
+//        ArryaSum arryaSum = new ArryaSum();
+        int[] arr={1,2,3,4};
+        int actulArray[] = arryaSum.getEvenArray(arr);
+        int expectedArry[]={2,4};
+        assertArrayEquals(actulArray,expectedArry);
     }
 
 
